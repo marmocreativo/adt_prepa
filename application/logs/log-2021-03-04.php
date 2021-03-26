14ce927fcfe6b97e85b96fd4620d2476835493d7
marmocreativo <marmocreativo@gmail.com>
2021-03-26 13:20:54 -0600

    debug

---
+++ application/logs/log-2021-03-04.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-04 21:30:49 --> 404 Page Not Found: Usuarios_Inicio/proyectos
+ERROR - 2021-03-04 21:31:09 --> 404 Page Not Found: Usuarios_Inicio/equipos
+ERROR - 2021-03-04 21:54:18 --> 404 Page Not Found: Admin_Tareas/detalles
