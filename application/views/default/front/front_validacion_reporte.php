12c26fdb447694492ea1351d0f6a5d4038be4eba
marmocreativo <marmocreativo@gmail.com>
2023-05-19 18:18:28 -0500

    reporte por dimensiones

--- application/views/default/front/front_validacion_reporte.php
+++ application/views/default/front/front_validacion_reporte.php
@@ -11,8 +11,9 @@ $dimensiones = $this->GeneralModel->lista('validacion_dimension','',['ID_LISTA'=
             <thead>
                 <tr>
                     <th>Tareas</th>
-                    <th>Resumen</th>
-                    <th>Dimensiones</th>
+                    <?php foreach($dimensiones as $dimension){ ?>
+                    <th><?php echo $dimension->TITULO; ?></th>
+                    <?php } ?>
                 </tr>
             </thead>
             <?php
@@ -24,39 +25,28 @@ $dimensiones = $this->GeneralModel->lista('validacion_dimension','',['ID_LISTA'=
                     <?php $detalles_tarea = $this->GeneralModel->detalles('tareas',['ID_TAREA'=>$revision->ID_TAREA]); ?>
                     <tr>
                         <td><?php echo $detalles_tarea['TAREA_TITULO']; ?></td>
-                        <?php
-                            $respuestas_totales = $this->GeneralModel->lista('validacion_respuesta','',['ID_REVISION'=>$revision->ID_REVISION,'ID_TAREA'=>$detalles_tarea['ID_TAREA']],'','','');
-                            $respuestas_validadas = $this->GeneralModel->lista('validacion_respuesta','',['ID_REVISION'=>$revision->ID_REVISION,'ID_TAREA'=>$detalles_tarea['ID_TAREA'],'VALOR'=>'validada'],'','','');
-                            $numero_totales = count($respuestas_totales);
-                            $numero_validadas = count($respuestas_validadas);
-                            $total_totales += $numero_totales;
-                            $total_validadas += $numero_validadas;
-                        ?>
+                        <?php foreach($dimensiones as $dimension){ ?>
                         <td>
-                            <?php echo $numero_validadas; ?> / <?php echo $numero_totales; ?><br>
-                            <?php echo ($numero_validadas / $numero_totales) * 100; ?>%<br>
+                            <?php
+                                $parametros = $this->GeneralModel->lista('validacion_parametros','',['ID_DIMENSION' =>$dimension->ID_DIMENSION],'','','');
+                                $respuestas = 0;
+                                $respuestas_validadas = 0;
+                                foreach($parametros as $parametro){
+                                    $detalles_respuesta = $this->GeneralModel->detalles('validacion_respuesta',['ID_REVISION'=>$revision->ID_REVISION,'ID_PARAMETRO'=>$parametro->ID_PARAMETRO]);
+                                    $respuestas ++;
+                                    if($detalles_respuesta['VALOR']=='validada'){
+                                        $respuestas_validadas ++;
+                                    }
+                                }
+                            ?>
+                            <?php echo ($respuestas_validadas*100)/$respuestas; ?>%
                         </td>
-                        <td>
-                            <table class="table">
-                                <tr>
-                                    <?php foreach($dimensiones as $dimension){ ?>
-                                        <?php $parametros = $this->GeneralModel->lista('validacion_parametros','',['ID_DIMENSION'=>$dimension->ID_DIMENSION],'','',''); ?> 
-                                        <?php foreach($parametros as $parametro){ ?>
-                                            <?php $respuesta = $this->GeneralModel->detalles('validacion_respuesta',['ID_REVISION'=>$revision->ID_REVISION,'ID_TAREA'=>$detalles_tarea['ID_TAREA'],'ID_PARAMETRO'=>$parametro->ID_PARAMETRO]); ?>
-                                            <td><?php echo $parametro->TITULO; ?> <?php if($respuesta['VALOR']=='validada'){ echo '<i class="fa fa-check text-success"></i>';}else{ echo '<i class="fa fa-times text-danger"></i>'; } ?></td>
-                                            
-                                        <?php } ?>
-                                    <?php } ?>
-                                </tr>
-                            </table>
-                        </td>
+                        <?php } ?>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
         <h4>Resultado final</h4>
-        <p><?php echo $total_validadas; ?> / <?php echo $total_totales; ?><br>
-            <?php echo round(($total_validadas / $total_totales) * 100,2); ?>%<br></p>
     </div>
     </div>
 </div>
