b8ffa80f3e76be9e189d91e2bd21eba4b53cb4bb
Andrés Ruelas <pixelbiff@gmail.com>
2023-05-25 18:08:57 -0600

    Ajustes en vista de proyectos y tareas
    Ajustes en vista de proyectos y tareas:
      - Se eliminan sombras de contenedores
      - Se disminuye espacio entre elementos de lista
      - Se cambia el tamaño del contenedor de 8 a 12 columnas en cistas de tareas y proyectos
      - En proyectos se cambia la vista a tarjetas

---
+++ application/logs/log-2023-05-22.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-05-22 21:49:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'polaris'@'localhost' (using password: YES) /Applications/MAMP/htdocs/adt_prepa/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2023-05-22 21:49:28 --> Unable to connect to the database
