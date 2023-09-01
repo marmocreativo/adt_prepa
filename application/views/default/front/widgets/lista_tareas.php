fa76889356b9a44557b6437cb28b969806ed8d5b
Manuel Marmolejo <marmocreativo@gmail.com>
2023-09-01 01:17:49 -0600

    debugs

--- application/views/default/front/widgets/lista_tareas.php
+++ application/views/default/front/widgets/lista_tareas.php
@@ -65,17 +65,33 @@ $icono = "far fa-clock";
           $tablas_join['usuarios'] = 'usuarios_tareas.ID_USUARIO = usuarios.ID_USUARIO';
           $parametros_and['usuarios_tareas.ID_TAREA'] = $tarea->ID_TAREA;
           $usuarios = $this->GeneralModel->lista_join('usuarios_tareas',$tablas_join,$parametros_or,$parametros_and,'','','','');
+         
+          $detalles_proceso_activo = $this->GeneralModel->detalles('roles_historial',['ID'=>$tarea->ID_PROCESO]);
+          $usuario_proceso = $this->GeneralModel->detalles('usuarios',['ID_USUARIO'=>$detalles_proceso_activo['ID_USUARIO']]);
+          $procesos_totales = $this->GeneralModel->conteo_elementos('roles_historial',['ID_TAREA'=>$tarea->ID_TAREA]);
+          $procesos_pendiente = $this->GeneralModel->conteo_elementos('roles_historial',['ID_TAREA'=>$tarea->ID_TAREA,'ESTADO'=>'pendiente']);
+          $procesos_completo = $this->GeneralModel->conteo_elementos('roles_historial',['ID_TAREA'=>$tarea->ID_TAREA,'ESTADO'=>'completo']);
         ?>
         <div class="d-flex justify-content-start align-items-center py-2">
           <small class="badge-tarea px-2 border <?php echo ' '.$color; ?> bg-opacity-25 me-3 rounded-pill"><i class="<?php echo $icono.' '; ?>"></i><?php echo ' '.$estado; ?></small>
           <a class="me-auto nombre-tarea" href="<?php echo base_url('index.php/tareas/detalles?id='.$tarea->ID_TAREA); ?>">
             <span title="<?php echo $tarea->ESTADO; ?>"> <?php echo $tarea->TAREA_TITULO; ?></span>
           </a>
+          <?php if(!empty($tarea->ID_PROCESO)){ ?>
           <a class="usuario-asignado text-muted pe-4">
-						<img src=" " title="Francisco Javier" width="50px" class="rounded-circle border border-secondary" alt="">
-						<span>Francisco Javier</span>
+						<img src=" <?php echo base_url('contenido/img/usuarios/default.jpg'); ?>" title="<?php echo $usuario_proceso['USUARIO_NOMBRE'].' '.$usuario_proceso['USUARIO_APELLIDOS']; ?>" width="50px" class="rounded-circle border border-secondary" alt="">
+						<span><?php echo $usuario_proceso['USUARIO_NOMBRE']; ?></span>
           </a>
-          <span class="pe-4 proceso-actual ">2/10 Bocetaje</span>
+          <?php }else{ ?>
+            <a class="usuario-asignado text-muted pe-4">
+              <span>N/A</span>
+            </a>
+            <?php } ?>
+            <?php if($procesos_totales>0){ ?>
+          <span class="pe-4 proceso-actual "><?php echo $procesos_completo; ?>/<?php echo $procesos_totales; ?> <?php echo $detalles_proceso_activo['ETIQUETA']; ?></span>
+              <?php }else{ ?>
+                <span class="pe-4 proceso-actual ">N/A</span>
+                <?php } ?>
           <button data-enlace="<?php echo base_url('index.php/tareas/borrar?id='.$tarea->ID_TAREA); ?>" class="ml-2 btn btn-outline-danger btn-sm borrar_entrada"> <i class="fas fa-trash"></i></button>
         </div>
 
